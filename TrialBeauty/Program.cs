de5154a7136d48e26501d16750c0ba0a50b8242e
Евгений Меркулов <i_zhenya.z@mail.ru>
2018-04-01 23:06:42 +0300

    Initital

---
+++ TrialBeauty/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrialBeauty
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool flag = false;
+            Console.ReadLine();
+            string s = Console.ReadLine();
+            if ((s[0] == '?') || (s[s.Length - 1] == '?'))
+                flag = true;
+            for (int i = 1; (i < s.Length - 1); i++)
+            {
+                if (s[i] == '?')
+                {
+                    if ((s[i - 1] == s[i + 1]) || (s[i + 1] == '?'))
+                        flag = true;
+                }
+                else
+                    if ((s[i] == s[i + 1]) || (s[i] == s[i - 1]))
+                {
+                    Console.WriteLine("No");
+                    return;
+                }
+            }
+            if (flag)
+                Console.WriteLine("Yes");
+            else
+                Console.WriteLine("No");
+            Console.ReadKey();  
+        }
+    }
+}
